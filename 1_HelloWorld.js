24add7ddb886bceab463a49571eef3a8e2c275c7
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-26 12:03:28 +0600

    Simple print a string literature

---
+++ 1_HelloWorld.js
@@ -0,0 +1,11 @@
+/**
+ * @file 1_HelloWorld.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Simple print a string literature
+ * @version 0.1
+ * @date 2023-01-26
+ * @since ThursDay; 12:02 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+console.log("Hello World!");
