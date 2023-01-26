ef574e2ee741209078d99608c1853148cbe546c2
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-26 12:34:14 +0600

    Types of variables using var key-word

---
+++ 2_Variables.js
@@ -0,0 +1,21 @@
+/**
+ * @file 2_Variables.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Types of variables using var key-word
+ * @version 0.1
+ * @date 2023-01-26
+ * @since ThursDay; 12:33 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+var numberType1 = 101;
+console.log(`numberType1: `, numberType1, typeof numberType1,'Typed'); /*Number type*/
+
+var numberType2 = 1.414213;
+console.log("numberType2: ", numberType2, typeof numberType2, 'Typed');/*Number type*/
+
+var boolType = true;
+console.log('boolType: ' , boolType, typeof boolType, 'Typed');
+
+var stringType = "Muhammad Yousuf Ali";
+console.log("stringType: ", stringType, typeof stringType, 'Typed');
