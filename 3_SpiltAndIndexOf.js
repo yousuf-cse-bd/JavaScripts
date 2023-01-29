f9b62984272d02b940d61c634050870e633a3bc4
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-29 22:30:04 +0600

    split and indexOf two built-in method

---
+++ 3_SpiltAndIndexOf.js
@@ -0,0 +1,13 @@
+/**
+ * @file 3_SpiltAndIndexOf.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief split and indexOf two built-in method
+ * @version 0.1
+ * @date 2023-01-29
+ * @since SunDay; 10:28 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+var friend = "Yousuf Tamim Maruf Istiak";
+console.log(friend.split("Yousuf"));
+console.log(friend.indexOf('Yousuf'));
