ba16ef980ca5db586a9411d5f8fca90e6993beeb
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-30 19:26:34 +0600

    To converting string to number typed of values

---
+++ 4_StringToNumbers.js
@@ -0,0 +1,19 @@
+/**
+ * @file 4_StringToNumbers.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief To converting string to number typed of values
+ * @version 0.1
+ * @date 2023-01-30
+ * @since MonDay; 07:25 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+var id = "171311101";
+id = parseInt(id);
+console.log("ID: ",id ,typeof id);
+
+var piValue = "3.14159";
+piValue = parseFloat(piValue); /*Need for double*/
+
+console.log("Pie-Value: ", piValue ,typeof piValue);
+
