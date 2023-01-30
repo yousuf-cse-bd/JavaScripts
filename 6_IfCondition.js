be8b494c03357e75afbef8b6904afc6e6d7fbd2a
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-30 22:40:59 +0600

    Direct print within condition's portion

---
+++ 6_IfCondition.js
@@ -0,0 +1,18 @@
+/**
+ * @file 6_IfCondition.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Direct print within condition's portion
+ * @version: 0.1
+ * @date 2023-01-30
+ * @since MonDay; 10:39 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+
+if(console.log("This is Yousuf")){}
+
+if(console.log("I'm Yousuf"));
+
+while(console.log("My name is Yousuf")){}
+
+while(console.log("The name of mine is Yousuf"));
