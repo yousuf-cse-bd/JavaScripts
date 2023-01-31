70c226d4010142181b7e5473003f50c42e12c363
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-01-31 12:18:25 +0600

    Three basic loops

---
+++ 9_Loops.js
@@ -0,0 +1,26 @@
+/**
+ * @file 9_Loops.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Three basic loops
+ * @version: 0.1
+ * @date 2023-01-31
+ * @since TuesDay; 12:18 AM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+for(; console.log("ForLoop1"); );
+for(; console.log("ForLoop2"); ){}
+
+var randomElements = ["Yousuf", 171311101, 3.35, "Tamim", "Maruf"];
+
+for(var i = 0; i < randomElements.length; i++){
+    console.log(randomElements[i]);
+}
+
+while(console.log("While-Loop1"));
+while(console.log("While-Loop2")){}
+
+do{
+
+}while(console.log("Do-While-Loop"));
+
