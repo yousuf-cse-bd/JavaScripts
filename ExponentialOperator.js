421282b05b6d8f354e0a3fc15cbe41381a3490f4
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-03-10 18:04:04 +0600

    Simple exponential operator and Math.pow method

---
+++ ExponentialOperator.js
@@ -0,0 +1,15 @@
+/**
+ * @file ExponentialOperator.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Simple exponential operator and Math.pow method
+ * @version 0.1
+ * @date 2023-03-09
+ * @since ThursDay; 06:00 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+let x = 2 ** 3; /* Exponential Operator: 2 ^ 3 */
+console.log(x);
+
+let y = Math.pow(2, 3); /* This is function: 2^3 */
+console.log(y);
