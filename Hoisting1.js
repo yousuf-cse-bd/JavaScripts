2c046627d4905a844f789de019d42e706ba38357
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-03-13 08:04:21 +0600

    JavaScipt supports hoisting but not recommend

---
+++ Hoisting1.js
@@ -0,0 +1,20 @@
+/**
+ * @file Hoisting1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief JavaScipt supports hoisting but not recommend
+ * @version 0.1
+ * @date 2023-03-13
+ * @since MonDay; 08:03 AM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+// var carName;
+// carName = undefined
+carName = 'BMW';
+console.log(carName);
+var carName;
+
+// let homeName;
+// homeName = 'Hell';
+// console.log(homeName);
+// let homeName;
