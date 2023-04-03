a9c2e3440110fcf619a6487cf70ae8fd10c8b778
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-03 17:21:03 +0600

    Array includes() - check present a particular element into array

---
+++ ArrayIncludes1.js
@@ -0,0 +1,13 @@
+/**
+ * @file ArrayIncludes1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Array includes() - check present a particular element into array
+ * @version 0.1
+ * @date 2023-04-03
+ * @since MonDay; 05:20 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(fruits.includes("Mango")); /*true*/
