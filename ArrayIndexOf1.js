fc1e05f1a25774e506a9ab53a1d64ee64e2663e8
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-03 17:14:50 +0600

    Array indexOf() and lastIndexOf

---
+++ ArrayIndexOf1.js
@@ -0,0 +1,18 @@
+/**
+ * @file ArrayIndexOf1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Array indexOf() and lastIndexOf
+ * @version 0.1
+ * @date 2023-04-03
+ * @since MonDay; 05:14 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+const fruits = ['Apple', 'Orange', 'Apple', 'Mango'];
+let position = fruits.indexOf("Apple");
+
+console.log(position);
+console.log("-------------------------------------");
+
+position = fruits.lastIndexOf('Apple');
+console.log(position);
