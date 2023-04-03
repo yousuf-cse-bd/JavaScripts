e3235e26e5ce40ba2a2fafd220648abd9a5b2cde
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-03 10:10:38 +0600

    Array map method for accessing array return something

---
+++ Reduce1.js
@@ -0,0 +1,20 @@
+/**
+ * @file Reduce1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Array map method for accessing array return something
+ * @version 0.1
+ * @date 2023-04-03
+ * @since MonDay; 10:07 AM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+const numbers1 = [45, 4, 9, 16, 25];
+
+const sum = numbers1.reduce(accessArray);
+
+/*Initially total = 0*/
+function accessArray(total, value, index, array){
+    return (total + value);
+}
+console.log(numbers1);
+console.log(`Sum: `+sum);
