9106122e1b8c106e80bf1dede3aad99a7bd8e78d
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-04 22:08:13 +0600

    The from() method returns an Array object from any object with a lenght or any iterable object.

---
+++ ArrayFrom1.js
@@ -0,0 +1,12 @@
+/**
+ * @file ArrayFrom1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief The from() method returns an Array object from any object with a lenght or any iterable object
+ * @version 0.1
+ * @date 2023-04-04
+ * @since TuesDay; 10:07 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+
+console.log(Array.from("ABCDEFG"));
