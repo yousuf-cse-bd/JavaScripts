d8360505c085f1c762be4c67f0dfbb71af3b4657
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-05 16:56:15 +0600

    The array sort() method can be changed existing array

---
+++ Problme1SortAlphabetically.js
@@ -0,0 +1,15 @@
+/**
+ * @file Problme1SortAlphabetically.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief The array sort() method can be changed existing array
+ * @version 0.1
+ * @date 2023-04-05
+ * @since WednesDay; 04:54
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+const students = ['Sumit', 'Saad', 'Daiyan', 'Akash'];
+console.log(students);
+
+students.sort();
+console.log(students);
