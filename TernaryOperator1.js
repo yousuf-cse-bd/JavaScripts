1fa09e10b02b5620cc9bb89915852903ec3c495e
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-08 22:03:48 +0600

    Check adulty using ternary operator

---
+++ TernaryOperator1.js
@@ -0,0 +1,12 @@
+/**
+ * @file TernaryOperator1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Check adulty using ternary operator
+ * @version 0.1
+ * @date 2023-04-08
+ * @since SaturDay; 10:02 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+const age = 26;
+console.log((age >= 18)? (age <= 40)?"You are an adult.":"You are old.":"You are young.");
