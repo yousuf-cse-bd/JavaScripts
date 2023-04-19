e7eda916827be41da4e3ee2ff00a24c919a2282a
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-19 23:01:15 +0600

    Declare and initiate at the begining of all variables

---
+++ InitializeVariables.js
@@ -0,0 +1,17 @@
+/**
+ * @file InitializeVariables.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Declare and initiate at the begining of all variables
+ * @version 0.1
+ * @date 2023-04-19
+ * @since WednesDay; 11:00 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+let firstName = "";
+let lastName = "";
+let price = 0;
+let dicount = 0;
+let fullPrice = 0;
+const myArray = [];
+const myObject = {};
