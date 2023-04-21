2c521a3ab97ca0f29ab3da6c505b18641bc5df28
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-04-21 08:42:51 +0600

    Check odd even using ternary operator and bit wsied and

---
+++ AwesomeBitWiseAnd.js
@@ -0,0 +1,16 @@
+/**
+ * @file AwesomeBitWiseAnd.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Check odd even using ternary operator and bit wsied and
+ * @version 0.1
+ * @date 2023-04-20
+ * @since FriDay; 08:41 AM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+oddEven = number =>{
+    console.log((number & 1)?"Odd":"Even");
+}
+
+oddEven(2);
+oddEven(3);
