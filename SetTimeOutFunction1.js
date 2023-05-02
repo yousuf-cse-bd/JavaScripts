2775b43bac5ff75fe7859caebda4240a00191f44
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-05-02 17:46:46 +0600

    JS Asynchronous behavior with setTimeOut()

---
+++ SetTimeOutFunction1.js
@@ -0,0 +1,15 @@
+/**
+ * @file SetTimeOutFunction1.js
+ * @author: Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief JS Asynchronous behavior with setTimeOut()
+ * @version 0.1
+ * @date 2023-05-02
+ * @since TuesDay; 05:44 PM
+ * @copyright Copyright (c) 2023
+ * @institute Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+console.log("Line 1");
+setTimeout(function(){
+    console.log("Line 2");
+}, 0);
+console.log("Line 3");
